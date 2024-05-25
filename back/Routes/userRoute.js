236100bfca8cc8a594cc5a3e24c258dbb0945dcb
Reza-Kh1 <r.khani1385.66@gmail.com>
2024-05-25 20:43:03 +0330

    clear

--- back/Routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const route = express.Router()
-route.route("/").get((req, res) => {
-    res.send({ ok })
-})
-export default route
--- back/server.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import dotenv from "dotenv";
-import app from "./app/app.js";
-dotenv.config();
-const server = http.createServer(app);
-server.listen(process.env.PORT_SERVER, process.env.HOST_NAME, () => {
-    console.log(`server run in port ${process.env.PORT_SERVER}`);
-});

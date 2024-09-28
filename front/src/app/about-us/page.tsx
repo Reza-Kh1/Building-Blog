c156806fe9a6095f9bfbcf8ea7b497c3c960fb1f
Reza-Kh1 <r.khani1385.66@gmail.com>
2024-09-29 00:24:20 +0330

    update about-us

--- front/src/app/about-us/page.tsx
+++ front/src/app/about-us/page.tsx
@@ -7,7 +7,48 @@ import ImgTag from "@/components/ImgTag/ImgTag";
 import { FaChevronLeft, FaHome } from "react-icons/fa";
 import { FcDepartment } from "react-icons/fc";
 import Image from "next/image";
-import { FaCheck } from "react-icons/fa6";
+import { FaCheck, FaPlay } from "react-icons/fa6";
+import Link from "next/link";
+const dataBanner = [
+  {
+    src: "/icon-paint.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "طراحی داخلی",
+  },
+  {
+    src: "/icon-home.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "ساخت هوشمندانه",
+  },
+  {
+    src: "/icon-check.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "اعتبار",
+  },
+  {
+    src: "/icon-money.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "ذخیره کردن پول",
+  },
+  {
+    src: "/icon-conflict.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "سرعت",
+  },
+  {
+    src: "/icon-worker.png",
+    text: "طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را توی خونه هاشون ارائه بدن",
+    title: "با تجربه",
+  },
+];
+const dataTrustUs = [
+  "بیش از 30 سال تجربه",
+  "تحویل پروژه در زمان معین شده",
+  "100% تضمین و اطمینان",
+  "تیمی حرفه ای که از 100+ تشکیل شده",
+  "پشتیبانی بی نظیر پس از اتمام پروژه",
+  "مشاوره درست و متناسب با شرایط شما",
+];
 export default function page() {
   const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
   return (
@@ -84,157 +125,85 @@ export default function page() {
       </div>
       <div className="w-full flex gap-3 bg-[#282828] shadow-md">
         <div className="w-1/3 flex items-center">
-          <figure>
-            <ImgTag src={"/5.jpg"} alt={"test"} width={700} height={600} />
-          </figure>
+          <ImgTag src={"/5.jpg"} alt={"test"} width={700} height={600} />
         </div>
-        <div className="w-1/3 flex flex-col gap-8 p-10">
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-paint.png"}
-                width={80}
-                height={30}
-              />
-            </figure>
-
-            <div>
-              <span className="text-slate-200">طراحی داخلی</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
+        <div className="w-2/3 grid grid-cols-2 gap-8 p-10">
+          {dataBanner.map((props, index) => (
+            <div className="flex gap-3" key={index}>
+              <figure className="text-4xl">
+                <Image alt="paint" src={props.src} width={80} height={30} />
+              </figure>
+              <div>
+                <span className="text-slate-200">{props.title}</span>
+                <p className="text-slate-400 text-sm mt-2 text-justify">
+                  {props.text}
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-home.png"}
-                width={80}
-                height={30}
-              />
-            </figure>
-            <div>
-              <span className="text-slate-200">ساخت هوشمندانه</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-check.png"}
-                width={80}
-                height={30}
-              />
-            </figure>
-            <div>
-              <span className="text-slate-200">اعتبار</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
-        <div className="w-1/3 flex flex-col gap-8 p-10">
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-money.png"}
-                width={80}
-                height={30}
+      </div>
+      <div className="max-w-7xl mx-auto w-full my-5 ">
+        <h3 className="text-xl font-semibold mb-3">برخی از پروژه های ما</h3>
+        <div className="grid grid-cols-3 gap-5">
+          <div>
+            <Link href={"#"} className="relative group">
+              <ImgTag
+                fiureClass="relative w-full overflow-hidden rounded-md"
+                alt={"نمونه پروژه"}
+                src={"/6.jpg"}
+                width={500}
+                height={350}
+                classPlus="hover-project"
               />
-            </figure>
-            <div>
-              <span className="text-slate-200">ذخیره کردن پول</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-conflict.png"}
-                width={80}
-                height={30}
-              />
-            </figure>
-            <div>
-              <span className="text-slate-200">سرعت</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-3">
-            <figure className="text-4xl">
-              <Image
-                alt="paint"
-                src={"/icon-worker.png"}
-                width={80}
-                height={30}
-              />
-            </figure>
-            <div>
-              <span className="text-slate-200">با تجربه</span>
-              <p className="text-slate-400 text-sm mt-2 text-justify">
-                طراحی های مدرن و سریع برای دوستانی که میخواهند تغییر های جدید را
-                توی خونه هاشون ارائه بدن
-              </p>
+              <i className="p-3 group-hover:opacity-100 transition-all opacity-0 hover:bg-gray-50 rounded-full bg-gray-200/80 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <FaPlay />
+              </i>
+            </Link>
+            <div className="flex flex-col gap-2 mt-3">
+              <div>
+                <Link href={"#"} className="inline-block">
+                  <span className="hover:text-blue-400">پروژه برج امید</span>
+                </Link>
+              </div>
+              <span className="text-sm text-gray-700">تهران ، الهیه</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="max-w-7xl mx-auto mt-5 w-full flex gap-3 items-center">
+      <div className="w-full my-10 py-5 bg-blue-400 shadow-md flex items-center gap-2 justify-center relative">
+        <span className="text-white">
+          آیا میخواهید خانه رویایی خود را با ما بسازید ؟
+        </span>
+        <Link href={"#"} className="text-white font-semibold">اینجا کلیک کنید</Link>
+        <span className="w-full absolute h-1 bg-blue-500/70 left-0 bottom-0"></span>
+      </div>
+      <div className="max-w-7xl mx-auto w-full flex gap-3 items-center">
         <div className="w-1/2">
           <h3 className="text-xl mb-3 font-semibold">
-            به حرفه ای ها بسپارید !
+            پروژه خود را به دست حرفه ای ها بسپارید !
           </h3>
           <h4 className="text-sm text-gray-600 mb-3">
             6 نکته ای که باعث میشود به ما اعتماد کنید
           </h4>
           <ul className="flex flex-col gap-2">
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>بیش از 30 سال تجربه</span>
-            </li>
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>تحویل پروژه در زمان معین شده</span>
-            </li>
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>100% تضمین و اطمینان</span>
-            </li>
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>تیم حرفه ای اختصاصی +100</span>
-            </li>
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>پشتیبانی بی نظیر پس از اتمام پروژه</span>
-            </li>
-            <li className="flex items-center gap-3">
-              <i><FaCheck /></i>
-              <span>مشاوره درست و متناسب با شرایط شما</span>
-            </li>
+            {dataTrustUs.map((i, index) => (
+              <li key={index} className="flex items-center gap-3">
+                <i>
+                  <FaCheck />
+                </i>
+                <span>{i}</span>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="w-1/2">
-          <figure>
-            <ImgTag src={"/about.jpg"} alt={"about-us"} width={500} height={450} />
-          </figure>
+          <ImgTag
+            src={"/about.jpg"}
+            alt={"about-us"}
+            width={500}
+            height={450}
+          />
         </div>
       </div>
     </div>
--- front/src/components/ImgTag/ImgTag.tsx
+++ front/src/components/ImgTag/ImgTag.tsx
@@ -1,41 +1,48 @@
-"use client"
-import Image from 'next/image'
-import React, { useState } from 'react'
-import LoadingImg from '../LoadingImg/LoadingImg';
-import ImageError from "@/../public/errorImage.png"
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import LoadingImg from "../LoadingImg/LoadingImg";
+import ImageError from "@/../public/errorImage.png";
 type ImageType = {
-    src: any;
-    alt: string | null;
-    className?: string;
-    width: number;
-    height: number;
-    classPlus?: string;
+  src: any;
+  alt: string | null;
+  className?: string;
+  width: number;
+  height: number;
+  classPlus?: string;
+  fiureClass?: string;
 };
 export default function ImgTag({
-    width, height, src, alt, className, classPlus
+  width,
+  height,
+  src,
+  alt,
+  className,
+  classPlus,
+  fiureClass,
 }: ImageType) {
-    const [load, setLoad] = useState<boolean>(true);
-    const [srcError, setSrcError] = useState<any>();
-    return (
-        <figure className="relative w-full">
-            <Image
-                width={width}
-                height={height}
-                loading="lazy"
-                // layout='responsive'
-                // placeholder="blur"
-                // blurDataURL="data:image/gif;base64,..."
-                onLoad={() => setLoad(false)}
-                src={srcError || src || ""}
-                alt={alt || ""}
-                className={
-                    className ||
-                    `${classPlus} rounded-md shadow-md  table mx-auto` ||
-                    "rounded-md shadow-md w-full table mx-auto object-fill"
-                }
-                onError={() => setSrcError(ImageError)}
-            />
-            {load && <LoadingImg />}
-        </figure>
-    )
+  const [load, setLoad] = useState<boolean>(true);
+  const [srcError, setSrcError] = useState<any>();
+  return (
+    <figure className={fiureClass || "w-full relative"}>
+      <Image
+        width={width}
+        height={height}
+        loading="lazy"
+        // layout='responsive'
+        // placeholder="blur"
+        // blurDataURL="data:image/gif;base64,..."
+        onLoad={() => setLoad(false)}
+        src={srcError || src || ""}
+        alt={alt || ""}
+        className={
+          className ||
+          `${classPlus} rounded-md shadow-md  table mx-auto` ||
+          "rounded-md shadow-md w-full table mx-auto object-fill"
+        }
+        onError={() => setSrcError(ImageError)}
+      />
+      {load && <LoadingImg />}
+    </figure>
+  );
 }
